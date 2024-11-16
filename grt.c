43b2e3a7d42becace610080b08968f0f7807b501
ThatOneGin <pedrodragoncrystal@gmail.com>
2024-11-16 19:51:00 -0300

    Usage message when no input file

--- grt.c
+++ grt.c
@@ -1,8 +1,14 @@
 #include "gvm.c"
 
+void usage() {
+  printf("Grt, a runtime for the pytharscript language.\n");
+  printf("Usage: ./grt <input.gvm>\n\n");
+  printf("Error: input file not specified.\n");
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
-    printf("Expected input file.\n");
+    usage();
     exit(1);
   }
   vm.running = true;
